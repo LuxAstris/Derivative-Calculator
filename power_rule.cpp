dbb25eba729efb15dcca399d5469b2ce5affcdd8
LuxAstris <43798261+LuxAstris@users.noreply.github.com>
2018-10-11 15:55:18 -0400

    Update power_rule.cpp

--- power_rule.cpp
+++ power_rule.cpp
@@ -9,9 +9,28 @@ float coeff;
 
 float n;
 
+float C;
+
+float constant_rule;
+
 float power_rule;
 
+int specific_function;
 
+float constant()
+{
+    cout << "Welcome to the constant multiple rule.\n";
+
+    cout << "Enter in the value of x, the coefficient of x";
+
+    cin >> x >> coeff;
+
+    constant_rule = coeff;
+
+    cout << " The derivative of your function evaluated at the given value x is " <<constant_rule;
+
+    return constant_rule;
+}
 
 float power()
 {
@@ -23,13 +42,23 @@ float power()
 
     power_rule = coeff * n * pow(x, n-1);
 
-    cout << power_rule;
+    cout << " The derivative of your function evaluated at the given value x is " << power_rule;
 
     return power_rule;
 }
 
 int main()
 {
-    power();
+    cout << "Which derivative rule would you like to use? : \n" << "1 Constant Multiple Rule \n 2 Power Rule";
+
+    cin >> specific_function;
+
+    if(specific_function == 1){
+      constant();
+    }
+
+    else{
+        power();
+    }
 
 }
